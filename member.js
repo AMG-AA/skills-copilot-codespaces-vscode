4241753ecdc82a380c4c0c85f2f1b570bb9c0cc5
Aarón Martínez García <aggarcia1504@gmail.com>
2025-03-09 08:29:21 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    var skills = ["HTML", "CSS", "JavaScript", "React", "Node.js"];
+    var name = "John Doe";
+    var age = 30;
+    var isMarried = false;
+    var skillsMember = {
+        name: name,
+        age: age,
+        isMarried: isMarried,
+        skills: skills
+    }
+    return skillsMember;
+}
