1a0109048a1fc1fc9492526bd09e330b9b8bf51e
Aarón Martínez García <aggarcia1504@gmail.com>
2025-03-09 08:39:05 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,17 +1,13 @@
-// create web server
+// Create web server
 // using express.js
-
 // import express module
 const express = require("express");
-
 // create an express instance
 const app = express();
-
 // create a route handler
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
 // start the server
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
